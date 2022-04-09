55666a1788af539a64b2970b34273b939e3f5917
Bogdan <bogdan_kolesov@bk.ru>
2022-04-09 14:40:37 +0600

    add: dependences

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 export default function App() {
   return (
     <View>
-
+      <Text>  App</Text>
     </View>
   );
 }
