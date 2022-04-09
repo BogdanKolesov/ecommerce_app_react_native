fe2d680bd4ccffaeb2fa73d08fec3692e62fbd36
Bogdan <bogdan_kolesov@bk.ru>
2022-04-09 12:36:43 +0600

    add: database

---
+++ data/images/data.js
@@ -0,0 +1,103 @@
+export const COLORS = {
+    white: '#ffffff',
+    black: '#000000',
+    green: '#00ac76',
+    red: '#c04345',
+    blue: '#0043f9',
+    backgroundLight: '#f0f0f3',
+    backgroundMedium: '#89b9b9',
+    backgroundDark: '#777777'
+}
+
+export const Items = [
+    {
+        id: 1,
+        category: 'product',
+        productName: 'Super cat',
+        productPrice: 599,
+        description: 'Some description some description some descriptionsome descriptionsome descriptionsome descriptionsome descriptionsome descriptionsome description. some descriptionsome description.some description.',
+        isOff: true,
+        offPrecentage: 10,
+        productImage: require('../images/products/1.png'),
+        productImageList: {
+            productImage: require('../images/products/1.png'),
+            productImage: require('../images/products/1.png'),
+            productImage: require('../images/products/1.png'),
+        }
+    },
+    {
+        id: 2,
+        category: 'product',
+        productName: 'Super cat',
+        productPrice: 231,
+        description: 'Some description some description some descriptionsome descriptionsome descriptionsome descriptionsome descriptionsome descriptionsome description. some descriptionsome description.some description.',
+        isOff: true,
+        offPrecentage: 10,
+        productImage: require('../images/products/1.png'),
+        productImageList: {
+            productImage: require('../images/products/1.png'),
+            productImage: require('../images/products/1.png'),
+            productImage: require('../images/products/1.png'),
+        }
+    },
+    {
+        id: 3,
+        category: 'product',
+        productName: 'Super cat',
+        productPrice: 555,
+        description: 'Some description some description some descriptionsome descriptionsome descriptionsome descriptionsome descriptionsome descriptionsome description. some descriptionsome description.some description.',
+        isOff: true,
+        offPrecentage: 10,
+        productImage: require('../images/products/1.png'),
+        productImageList: {
+            productImage: require('../images/products/1.png'),
+            productImage: require('../images/products/1.png'),
+            productImage: require('../images/products/1.png'),
+        }
+    },
+    {
+        id: 4,
+        category: 'product',
+        productName: 'Super cat',
+        productPrice: 629,
+        description: 'Some description some description some descriptionsome descriptionsome descriptionsome descriptionsome descriptionsome descriptionsome description. some descriptionsome description.some description.',
+        isOff: true,
+        offPrecentage: 10,
+        productImage: require('../images/products/1.png'),
+        productImageList: {
+            productImage: require('../images/products/1.png'),
+            productImage: require('../images/products/1.png'),
+            productImage: require('../images/products/1.png'),
+        }
+    },
+    {
+        id: 5,
+        category: 'product',
+        productName: 'Super cat',
+        productPrice: 31239,
+        description: 'Some description some description some descriptionsome descriptionsome descriptionsome descriptionsome descriptionsome descriptionsome description. some descriptionsome description.some description.',
+        isOff: true,
+        offPrecentage: 10,
+        productImage: require('../images/products/1.png'),
+        productImageList: {
+            productImage: require('../images/products/1.png'),
+            productImage: require('../images/products/1.png'),
+            productImage: require('../images/products/1.png'),
+        }
+    },
+    {
+        id: 6,
+        category: 'product',
+        productName: 'Super cat',
+        productPrice: 1314669,
+        description: 'Some description some description some descriptionsome descriptionsome descriptionsome descriptionsome descriptionsome descriptionsome description. some descriptionsome description.some description.',
+        isOff: true,
+        offPrecentage: 10,
+        productImage: require('../images/products/1.png'),
+        productImageList: {
+            productImage: require('../images/products/1.png'),
+            productImage: require('../images/products/1.png'),
+            productImage: require('../images/products/1.png'),
+        }
+    }
+]
