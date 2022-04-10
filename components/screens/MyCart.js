eb1df90ad6be2f0c6ab6cefae16288cedfbce5bd
Bogdan <bogdan_kolesov@bk.ru>
2022-04-10 21:55:08 +0600

    fix: Cart

--- components/screens/MyCart.js
+++ components/screens/MyCart.js
@@ -46,6 +46,10 @@ const MyCart = ({ navigation }) => {
         setTotal(total)
     }
 
+    const removeItemFromCart = (id) => {
+        console.log(id)
+    }
+
     const renderProducts = (data, index) => {
         return (
             <TouchableOpacity key={index} style={{
@@ -73,7 +77,95 @@ const MyCart = ({ navigation }) => {
                         }}
                     />
                 </View>
-            </TouchableOpacity>
+                <View style={{
+                    flex: 1,
+                    height: '100%',
+                    justifyContent: 'space-around'
+                }}>
+                    <View style={{
+
+                    }}>
+                        <Text
+                            onPress={() => navigation.navigate('Home')}
+                            style={{
+                                fontSize: 16,
+                                maxWidth: '100%',
+                                color: COLORS.black,
+                                fontWeight: 'bold',
+                                letterSpacing: 1
+                            }}>
+                            {data.productName}
+                        </Text>
+                        <View style={{
+                            marginTop: 4,
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            opacity: 0.6
+                        }}>
+                            <Text style={{
+                                fontSize: 14,
+                                fontWeight: '400',
+                                maxWidth: '85%',
+                                marginRight: 4
+                            }}>
+                                &#8377; {data.productPrice}
+                            </Text>
+                            <Text>
+                                (&#8377;{(data.productPrice / 100 * 2) + data.productPrice})
+                            </Text>
+                        </View>
+                    </View>
+                    <View style={{
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        alignItems: 'center'
+                    }} >
+                        <View style={{
+                            flexDirection: 'row',
+                            alignItems: 'center'
+                        }}>
+                            <View style={{
+                                borderRadius: 100,
+                                marginRight: 20,
+                                padding: 4,
+                                borderWidth: 1,
+                                borderColor: COLORS.backgroundMedium,
+                                opacity: 0.5
+                            }}>
+                                <MaterialCommunityIcons name='minus' style={{
+                                    fontSize: 16,
+                                    color: COLORS.backgroundDark,
+                                }} />
+                            </View>
+                            <Text>1</Text>
+                            <View style={{
+                                borderRadius: 100,
+                                marginLeft: 20,
+                                padding: 4,
+                                borderWidth: 1,
+                                borderColor: COLORS.backgroundMedium,
+                                opacity: 0.5
+                            }}>
+                                <MaterialCommunityIcons name='plus' style={{
+                                    fontSize: 16,
+                                    color: COLORS.backgroundDark,
+                                }} />
+                            </View>
+                        </View>
+                        <TouchableOpacity onPress={() => removeItemFromCart(data.id)}>
+                            <MaterialCommunityIcons name='delete-outline'
+                                style={{
+                                    fontSize: 20,
+                                    color: COLORS.backgroundDark,
+                                    backgroundColor: COLORS.backgroundLight,
+                                    padding: 8,
+                                    borderRadius: 100,
+                                }}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </TouchableOpacity >
         )
     }
 
